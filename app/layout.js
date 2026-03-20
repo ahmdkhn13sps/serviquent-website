955491b46632ebca741ae82b37f0de08fe5fdd6e
Mohd Ahmad <mahmad@amsysis.com>
2026-03-20 09:11:42 +0530

    add Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Telecom Infrastructure Engineering | Serviquent Prime Solutions",
@@ -61,6 +62,19 @@ export default function RootLayout({ children }) {
         <meta name="geo.region" content="US" />
         <meta name="geo.placename" content="Cheyenne, Wyoming" />
         <meta name="language" content="English" />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2H6TM2DHGT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2H6TM2DHGT');
+          `}
+        </Script>
       </head>
       <body>{children}</body>
     </html>
