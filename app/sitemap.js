c23e6f8b09ddd6a80e8676e79fddd93886885b1c
ahmdkhn13sps <ahmdkhn13@gmail.com>
2026-04-09 10:44:37 +0530

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,7 +3,7 @@ export default function sitemap() {
     {
       url: "https://www.serviquent.com",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
@@ -11,6 +11,42 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
+    },
+    {
+      url: "https://www.serviquent.com/global-solutions",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.serviquent.com/#services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.serviquent.com/#engineering",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.serviquent.com/#fttx",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.serviquent.com/#about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.serviquent.com/#contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
     },
   ];
 }
